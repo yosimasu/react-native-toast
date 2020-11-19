8ea83458ae81ae327a822deda80dcfbf4764c8ad
Masu Lin <yosimasu@gmail.com>
2020-11-20 00:25:11 +0800

    ios: show
    1. cd packages/mobile/ios; npx pod-install; cd -

--- packages/react-native-toast/ios/Toast.m
+++ packages/react-native-toast/ios/Toast.m
@@ -1,6 +1,31 @@
 #import "Toast.h"
+#import "IOSNativeToast.h"
+
+@interface Toast()
+
+@property (nonatomic, retain) IOSNativeToast *toast;
+
+@end
 
 @implementation Toast
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.toast = [[IOSNativeToast alloc] init];
+    }
+    return self;
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
 
 RCT_EXPORT_MODULE()
 
@@ -8,6 +33,11 @@ RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnu
 {
     // TODO: Implement some actually useful functionality
     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+}
+
+RCT_EXPORT_METHOD(show:(NSString *)text)
+{
+    [self.toast showToast:text];
 }
 
 @end
